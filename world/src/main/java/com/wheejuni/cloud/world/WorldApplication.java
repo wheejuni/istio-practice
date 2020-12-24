f4120d4b0367489c4fe736cb6ce1f959a85fe18f
wheejuni <wheejuni@gmail.com>
2020-12-24 15:24:42 +0900

    world api 생성

--- world/src/main/java/com/wheejuni/cloud/world/WorldApplication.java
+++ world/src/main/java/com/wheejuni/cloud/world/WorldApplication.java
@@ -2,12 +2,25 @@ package com.wheejuni.cloud.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorldApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/v1/world")
+    static class WorldController {
+
+        @GetMapping
+        public String getWorld() {
+            return "world";
+        }
     }
 
 }
