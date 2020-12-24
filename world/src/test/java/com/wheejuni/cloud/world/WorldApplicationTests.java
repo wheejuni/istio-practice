5d91930aff4d1c1cdabd995e56f90d038b42c995
wheejuni <wheejuni@gmail.com>
2020-12-24 15:18:43 +0900

    project bootstrap

---
+++ world/src/test/java/com/wheejuni/cloud/world/WorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheejuni.cloud.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
